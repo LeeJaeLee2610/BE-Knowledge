0b31e2f8bbe335d41ab5562fe93f4480fd08b0c9
LeeJaeLee2610 <cuonggaayf01@gmail.com>
2023-02-01 19:28:29 +0700

    Update 1/2/2023

--- nestJSTypeORMTour/src/app.module.ts
+++ nestJSTypeORMTour/src/app.module.ts
@@ -1,7 +1,11 @@
+import { Task } from './task.entity';
+import { Employee } from './employee.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
+import { ContractInfo } from './contract-info.entity';
+import { Meeting } from './meeting.entity';
 
 @Module({
-  imports: [TypeOrmModule.forRoot({type: 'mysql', host: 'localhost', port: 3306, username: 'root', password: 'emcuong', database: 'typeorm', entities: [], synchronize: true})],
+  imports: [TypeOrmModule.forRoot({type: 'mysql', host: 'localhost', port: 3306, username: 'root', password: 'emcuong', database: 'typeorm', entities: [Employee, ContractInfo, Task, Meeting], synchronize: true})],
 })
 export class AppModule {}
--- nestJSTypeORMTour/src/employee.entity.ts
+++ nestJSTypeORMTour/src/employee.entity.ts
@@ -1,6 +1,11 @@
+import { JoinTable, ManyToMany } from 'typeorm';
+import { ManyToOne } from 'typeorm';
+import { OneToMany } from 'typeorm';
 import { ContractInfo } from './contract-info.entity';
 import { Column, OneToOne, PrimaryGeneratedColumn } from 'typeorm';
 import { Entity } from 'typeorm';
+import { Task } from './task.entity';
+import { Meeting } from './meeting.entity';
 
 @Entity()
 export class Employee {
@@ -10,6 +15,20 @@ export class Employee {
     @Column()
     name: string;
 
+    @ManyToOne(() => Employee, employee => employee.directReports, {onDelete: 'SET NULL'})
+    manager: Employee;
+
+    @OneToMany(() => ContractInfo, (contractInfo) => contractInfo.employee)
+    directReports: Employee[]
+
     @OneToOne(() => ContractInfo, (contractInfo) => contractInfo.employee)
     contractInfo: ContractInfo
+
+    @OneToMany(() => Task, task => task.employee)
+    tasks: Task[]
+
+    @ManyToMany(() => Meeting, (meeting) => meeting.attendees)
+    @JoinTable()
+    meetings: Meeting[];
+
 }
