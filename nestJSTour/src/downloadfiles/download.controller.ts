353929f15e6250f78ee2044561ae197dd2dcec4d
LeeJaeLee2610 <cuonggaayf01@gmail.com>
2023-02-07 21:36:34 +0700

    Update 7/2/2023

--- nestJSTour/src/downloadfiles/download.controller.ts
+++ nestJSTour/src/downloadfiles/download.controller.ts
@@ -9,6 +9,7 @@ import {
   import { DownloadService } from './download.service';
   import { Response } from 'express';
   import { LoggingInterceptor } from './logging.interceptor';
+import { ApiFileResponse } from './api-file-response.decoartor';
   
   @UseInterceptors(LoggingInterceptor)
   @Controller('download')
@@ -16,12 +17,30 @@ import {
   export class DownloadController {
     constructor(private readonly downloadService: DownloadService) {}
   
+    // @ApiResponse({
+    //   schema: {
+    //     type: 'string',
+    //     format: 'binary'
+    //   },
+    //   status: HttpStatus.OK
+    // })
+    // @ApiProduces('image/png')
+    // @Get('buffer')
+    // buffer(@Res() response: Response) {
+    //   const file = this.downloadService.imageBuffer();
+    //   response.contentType('image/png');
+    //   // response.attachment();
+    //   response.send(file);
+    // }
+  
+    @ApiFileResponse('image/png')
     @Get('buffer')
     buffer(@Res() response: Response) {
       const file = this.downloadService.imageBuffer();
+      response.contentType('image/png');
       response.send(file);
     }
-  
+    
     @Get('stream')
     stream(@Res() response: Response) {
       const file = this.downloadService.imageStream();
